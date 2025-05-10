7b097070158ae372b431f734afc802082f0f7d6f
Ivan Perevala <ivan95perevala@gmail.com>
2025-05-10 02:50:44 +0200

    Refactor module logic to register decorator
    # NOTE: IMPORTANT UPDATE

--- src/bhqrprt/_bl.py
+++ src/bhqrprt/_bl.py
@@ -10,12 +10,14 @@ import os
 import pprint
 import textwrap
 import time
-from typing import Any, Callable
+from typing import Any, Callable, Type
 
 import bpy
 from bpy.app.translations import pgettext
-from bpy.props import EnumProperty
-from bpy.types import Context, Operator, UILayout, bpy_prop_array, bpy_struct
+from bpy.props import EnumProperty, PointerProperty, IntProperty, StringProperty
+from bpy.types import Context, Operator, UILayout, bpy_prop_array, bpy_struct, AddonPreferences, PropertyGroup
+
+from . import _reports
 
 __all__ = (
     "report_and_log",
@@ -24,7 +26,13 @@ __all__ = (
     "update_log_setting_changed",
     "get_prop_log_level",
     "template_ui_draw_paths",
+    "template_submit_issue",
+    "register_reports",
+    "unregister_reports",
 )
+
+_CUR_DIR = os.path.dirname(__file__)
+_ICONS_DIR = os.path.join(_CUR_DIR, "icons")
 
 
 def report_and_log(
@@ -155,6 +163,7 @@ def log_settings(log: logging.Logger, *, item: bpy_struct) -> None:
     :param item: Structure.
     :type item: `bpy_struct`_
     """
+
     for prop in item.bl_rna.properties:
         identifier = prop.identifier
         if identifier != 'rna_type':
@@ -187,6 +196,18 @@ def update_log_setting_changed(log: logging.Logger, identifier: str) -> Callable
     return _log_setting_changed
 
 
+def _get_logger_stream_handler(log: logging.Logger) -> None | logging.StreamHandler:
+    for handler in log.root.handlers:
+        if isinstance(handler, logging.StreamHandler):
+            return handler
+
+
+def _get_logger_file_handler(log: logging.Logger) -> None | logging.FileHandler:
+    for handler in log.root.handlers:
+        if isinstance(handler, logging.FileHandler):
+            return handler
+
+
 def get_prop_log_level(log: logging.Logger, *, identifier: str) -> bpy.types.EnumProperty:
     """Property to be used in extension preferences that sets the log level.
 
@@ -197,57 +218,39 @@ def get_prop_log_level(log: logging.Logger, *, identifier: str) -> bpy.types.Enu
     :return: Property.
     :rtype: `EnumProperty`_
     """
-    _update_log_log_level = update_log_setting_changed(log, identifier=identifier)
+
+    name_to_level = {
+        'CRITICAL': logging.CRITICAL,
+        'ERROR': logging.ERROR,
+        'WARNING': logging.WARNING,
+        'INFO': logging.INFO,
+        'DEBUG': logging.DEBUG,
+    }  # NOTE: logging.getLevelNamesMapping() is not suitable here, it contains duplicates which are simplier remove here.
 
     def _update_log_level(self, context: Context):
-        for handle in log.root.handlers:
-            if type(handle) == logging.StreamHandler:
-                handle.setLevel(getattr(self, identifier))
+        value = getattr(self, identifier)
 
-        _update_log_log_level(self, context)
+        if stream_handler := _get_logger_stream_handler(log):
+            stream_handler.setLevel(value)
+
+        log.log(level=name_to_level[value], msg=f"Log level was set to {value}")
+
+    items = [
+        (
+            _name,
+            _name.capitalize(),
+            f"{_name.capitalize()} messages",
+            bpy.app.icons.new_triangles_from_file(os.path.join(_ICONS_DIR, f"{_name.lower()}.dat")),
+            _level
+        ) for _name, _level in name_to_level.items()
+    ]
 
     return EnumProperty(
-        items=(
-            (
-                logging.getLevelName(logging.DEBUG),
-                "Debug",
-                "Debug messages (low priority)",
-                0,
-                logging.DEBUG,
-            ),
-            (
-                logging.getLevelName(logging.INFO),
-                "Info",
-                "Informational messages",
-                0,
-                logging.INFO,
-            ),
-            (
-                logging.getLevelName(logging.WARNING),
-                "Warning",
-                "Warning messages (medium priority)",
-                0,
-                logging.WARNING,
-            ),
-            (
-                logging.getLevelName(logging.ERROR),
-                "Error",
-                "Error messages (high priority)",
-                0,
-                logging.ERROR,
-            ),
-            (
-                logging.getLevelName(logging.CRITICAL),
-                "Critical",
-                "Critical error messages",
-                0,
-                logging.CRITICAL,
-            ),
-        ),  # type: ignore
-        default=logging.getLevelName(logging.WARNING),
+        items=items,
+        default=logging.WARNING,
         update=_update_log_level,
         options={'SKIP_SAVE'},
-        translation_context='BHQAB_Preferences',
+        translation_context='bhqrprt',
         name="Log Level",
         description=(
             "The level of the log that will be output to the console. For log to file, this level value will "
@@ -267,13 +270,9 @@ def template_ui_draw_paths(log: logging.Logger, layout: UILayout, *, msgctxt: st
     :type msgctxt: str
     """
 
-    filepath: None | str = None
+    if handler := _get_logger_file_handler(log):
+        filepath = handler.baseFilename
 
-    for handler in log.root.handlers:
-        if isinstance(handler, logging.FileHandler):
-            filepath = handler.baseFilename
-
-    if filepath:
         directory, filename = os.path.split(filepath)
 
         layout.operator(
@@ -286,3 +285,160 @@ def template_ui_draw_paths(log: logging.Logger, layout: UILayout, *, msgctxt: st
             operator="wm.path_open",
             text=pgettext("Open Log: \"{filename}\"", msgctxt=msgctxt).format(filename=filename),
         ).filepath = filepath
+
+
+class _SubmitIssueRegistry:
+    BHQRPRT_OT_submit_issue: None | Type[Operator] = None
+    icon_value: int = 0
+
+    @classmethod
+    def ensure_register_submit_issue_operator(cls, log: logging.Logger):
+        if cls.BHQRPRT_OT_submit_issue:
+            return
+
+        name = log.name.replace('.', '_')
+
+        handler = _get_logger_file_handler(log)
+        if handler:
+
+            def _execute(self, context: Context):
+                bpy.ops.wm.url_open('EXEC_DEFAULT', url=self.url)
+                bpy.ops.wm.path_open('EXEC_DEFAULT', filepath=handler.baseFilename)
+                return {'FINISHED'}
+
+            cls.BHQRPRT_OT_submit_issue = type(
+                f"BHQRPRT_OT_{name}_submit_issue",
+                (Operator,),
+                dict(
+                    bl_idname=f"bhqrprt.{name}",
+                    bl_label="Submit Issue",
+                    bl_descriprion="Open issues page in browser and current log file",
+                    __annotations__=dict(url=StringProperty(options={'HIDDEN', 'SKIP_SAVE'},)),
+                    execute=_execute
+                )
+            )
+
+            bpy.utils.register_class(cls.BHQRPRT_OT_submit_issue)
+
+            cls.icon_value = bpy.app.icons.new_triangles_from_file(os.path.join(_ICONS_DIR, "issue.dat"))
+
+    @classmethod
+    def unregister_submit_issue_operator(cls):
+        if cls.BHQRPRT_OT_submit_issue:
+            bpy.utils.unregister_class(cls.BHQRPRT_OT_submit_issue)
+            cls.BHQRPRT_OT_submit_issue = None
+
+
+def template_submit_issue(layout: UILayout, url: str):
+    col = layout.column(align=False)
+    col.alert = True
+    col.scale_y = 1.5
+
+    props = col.operator(
+        operator=_SubmitIssueRegistry.BHQRPRT_OT_submit_issue.bl_idname,
+        icon_value=_SubmitIssueRegistry.icon_value
+    )
+    props.url = url
+
+
+class _LogSettingsRegistry:
+    BHQRPRT_log_settings: None | Type[PropertyGroup] = None
+
+    @classmethod
+    def register_log_settings_class(cls, log: logging.Logger):
+        if cls.BHQRPRT_log_settings:
+            bpy.utils.unregister_class(cls.BHQRPRT_log_settings)
+
+        name = log.name.replace('.', '_')
+
+        cls.BHQRPRT_log_settings = type(
+            f"BHQRPRT_{name}_log_settings",
+            (PropertyGroup,),
+            {
+                "__annotations__": {
+                    "log_level": get_prop_log_level(log, identifier="log_level"),
+                    "max_num_logs": IntProperty(
+                        min=1,
+                        soft_min=5,
+                        soft_max=100,
+                        default=30,
+                        options={'SKIP_SAVE'},
+                        name="Max Number of Log Files",
+                        description=(
+                            "Max number of log files in logs directory. "
+                            "Older files would be deleted if number of log files is greater than this value"
+                        )
+                    )
+                }
+            }
+        )
+        bpy.utils.register_class(cls.BHQRPRT_log_settings)
+
+    @classmethod
+    def unregister_log_settings_class(cls):
+        if cls.BHQRPRT_log_settings:
+            bpy.utils.unregister_class(cls.BHQRPRT_log_settings)
+            cls.BHQRPRT_log_settings = None
+
+
+def register_reports(log: logging.Logger, pref_cls: Type[AddonPreferences], directory: str):
+    def _draw_log_settings_helper(draw_func):
+        def _draw_wrapper(self, context: Context):
+            draw_func(context)
+
+            assert context.preferences
+
+            if context.preferences.view.show_developer_ui:
+                layout: UILayout = self.layout
+                col = layout.column(align=True)
+
+                idname = f"bhqrprt_{pref_cls.__name__.lower()}_reports"
+                header, panel = col.panel(idname, default_closed=True)
+                header.label(
+                    text="Reports",
+                    icon_value=header.enum_item_icon(self.bhqrprt, "log_level", self.bhqrprt.log_level)
+                )
+                if panel:
+                    row = panel.row(align=True)
+                    row.prop(self.bhqrprt, "log_level")
+                    panel.prop(self.bhqrprt, "max_num_logs")
+                    template_ui_draw_paths(log, panel, msgctxt="")
+
+        return _draw_wrapper
+
+    def _register_helper(register):
+        def _register():
+            _reports.setup_logger(log=log, directory=directory)
+
+            _LogSettingsRegistry.register_log_settings_class(log)
+            pref_cls.__annotations__['bhqrprt'] = PointerProperty(type=_LogSettingsRegistry.BHQRPRT_log_settings)
+
+            register()
+
+            pref = bpy.context.preferences
+            assert pref
+            addon = pref.addons.get(pref_cls.bl_idname, None)
+            if addon:
+                addon_pref = addon.preferences
+                if addon_pref:
+                    setattr(pref_cls, "draw", _draw_log_settings_helper(getattr(addon_pref, "draw")))
+
+                    if value := addon_pref.bhqrprt.log_level:
+                        if stream_handler := _get_logger_stream_handler(log):
+                            stream_handler.setLevel(value)
+
+                        _reports.purge_old_logs(directory=directory, max_num_logs=addon_pref.bhqrprt.max_num_logs)
+
+                    _SubmitIssueRegistry.ensure_register_submit_issue_operator(log)
+
+        return _register
+
+    return _register_helper
+
+
+def unregister_reports(unregister):
+    def _unregister():
+        unregister()
+        _LogSettingsRegistry.unregister_log_settings_class()
+
+    return _unregister
