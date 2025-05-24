399bf96a4dcc139ad2f558b67d938e0c721abb5f
Ivan Perevala <ivan95perevala@gmail.com>
2025-05-24 16:31:03 +0200

    First "real" test case;)

--- tests/test_reports.py
+++ tests/test_reports.py
@@ -1,8 +1,22 @@
 import os
 import shutil
+import subprocess
 
 
 def test_one():
     blender = shutil.which('blender', path=os.environ.get("BLENDER_DIR"))
 
-    assert blender
+    cli = [
+        blender,
+        "--background",
+        "--python-expr",
+        "import bpy; bpy.ops.object.bhqrprt_test('EXEC_DEFAULT)",
+        "--python-exit-code",
+        "255",
+    ]
+    proc = subprocess.Popen(cli, env=os.environ)
+
+    while proc.poll() is None:
+        pass
+
+    assert proc.returncode == 0
